73b35ce11e5d7e56ee23749994791ce4021c8edb
Nikita <103855729+nvmediagithub@users.noreply.github.com>
2025-04-16 22:21:48 +0300

    start refactor

--- Main.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func _ready():
-	var editor = $ImageEditor
-	var generator = $MeshGenerator
-	var visual = $MeshInstance3D
-
-	editor.connect("image_updated", Callable(generator, "update_mesh"))
-	generator.connect("mesh_updated", func(mesh): visual.mesh = mesh)
